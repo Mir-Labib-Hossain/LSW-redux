99e807e8c466786a29bf4c46ff62b9b626783155
Mir-Labib-Hossain <mirlabibhossain99@gmail.com>
2022-09-19 11:05:06 +0600

    1.8

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,8 +4,8 @@ const Counter: React.FC<{ id: number; count: number; inc: any; dec: any }> = ({ 
   return (
     <>
       <h1>Counter {id} :{count} </h1>
-      <Button handler={()=>inc(id)}>Increment</Button>
-      <Button handler={()=>dec(id)}>Decrement</Button>
+      <Button handler={()=>inc(id)}  >Increment</Button>
+      <Button handler={()=>dec(id)}  >Decrement</Button>
     </>
   );
 };
